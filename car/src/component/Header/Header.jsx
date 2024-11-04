fc3f980abf5f29ed7a6a7b399fdbb6a9fbc72ecf
abdullah345 <abdallahmohamed54655@gmail.com>
2024-11-04 23:33:59 +0200

    v5

--- car/src/component/Header/Header.jsx
+++ car/src/component/Header/Header.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
-
+import {Container,Row,Col} from 'reactstrap'
 const Header = () => {
   return (
-    <div>
-      
+    <header className='header'> 
+    <div className="header__top">
+        <Container>
+            <Row>
+                <Col ld='6' md='6' sm='6'>
+                <div className="header__top__left">
+                    
+                </div>
+                </Col>
+                <Col ld='6' md='6' sm='6'>
+
+                </Col>  
+            </Row>
+        </Container>
     </div>
+    </header>
   );
 }
 
